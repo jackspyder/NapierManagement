4cae247f6ce5313bd941be1d1ef3465d9fa4a10a
Jack Powell <jackspider@hotmail.com>
2017-10-25 19:44:47 +0100

    Reverted to here

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -1,27 +1,22 @@
 <?php
-
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-
 class Course extends Model
 {
     use SoftDeletes;
-
     protected $fillable = [
-      'price',
-      'duration',
-      'title',
-      'qualification',
-      'awarded_by',
-      'overview',
-      'description',
-      'who_for',
-      'requirements',
-      'career_path'
+        'price',
+        'duration',
+        'title',
+        'qualification',
+        'awarded_by',
+        'overview',
+        'description',
+        'who_for',
+        'requirements',
+        'career_path'
     ];
-
     public function lessons()
     {
         return $this->hasMany(Lesson::class);
